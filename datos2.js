0cf34c0a16a663db7dec69612adba250a311f71e
paola <paolms3@gmail.com>
2023-01-31 12:28:06 -0300

    comentarios

---
+++ datos2.js
@@ -0,0 +1,12 @@
+var persona={
+    nombre:"Jennie",
+    apellido:"Nichols",
+    fecha_de_nacimiento:"12/04/1990",
+    email:"jennie.n@gmail.com",
+    telefono:"3512089076",
+    direccion:"Santa Fé-Argentina",
+}
+console.log(persona.nombre)
+
+document.getElementById("nombre").innerHTML=persona.nombre
+document.getElementById("apellido").innerHTML=persona.apellido
